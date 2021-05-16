ee6a8e3e9b712b264ffe2a35e0f48c382512cc14
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2021-05-15 21:01:49 -0300

    Update down_ytube.py

--- down_ytube.py
+++ down_ytube.py
@@ -3,6 +3,10 @@ from pytube import Playlist
 import os
 import subprocess
 
+# exemplo
+# vid = 'c:\pasta1\pasta2\ '
+# sim, o espaço é importante
+
 vid = 'caminho_para_download'
 
 opc = int(input("1 = playlist\n2 = videos soltos\n->"))
@@ -11,7 +15,7 @@ opc = int(input("1 = playlist\n2 = videos soltos\n->"))
 #BAIXAR PLAYLIST
 
 if(opc==1):
-	form = int(input("\t0 = video\n\t1 = audio\n\t-> "))
+	form = int(input("\t0 = Retorno em video\n\t1 = Retorno em audio\n\t-> "))
 
 	#CRIAR PASTA PARA GUARDAR ARQUIVOS BAIXADOS
 	name = input("Nome da pasta (Sem separar caracteres)\n-> ")
@@ -24,7 +28,7 @@ if(opc==1):
 	#DOWNLOAD EM .MP4
 	if(form == 0):
 		for video in playl.videos:
-			print('\n\tBaixando : {} -> {} \n'.format(video.title, video.watch_url))
+			print('\n\tBaixando -> {} -> {} \n'.format(video.title, video.watch_url))
 			video.streams.\
 			filter(type='video', progressive=True, file_extension='mp4').\
 			order_by('resolution').\
@@ -35,7 +39,7 @@ if(opc==1):
 	#DOWNLOAD EM .MP4 SOMENTE AUDIO
 	if(form == 1):
 		for video in playl.videos:
-			print('\n\tBaixando : {} -> {} \n'.format(video.title, video.watch_url))
+			print('\n\tBaixando -> {} -> {} \n'.format(video.title, video.watch_url))
 			audio = video.streams.get_audio_only()
 			audio.download(vid)
 	if(form>1):
@@ -60,7 +64,7 @@ if(opc==2):
 	res += rep*[0]
 
 
-	format = int(input("\t1 = Retorno em .mp4, 2 = Retorno em .mp3?\n->"))
+	format = int(input("\t1 = Retorno em video, 2 = Retorno em audio?\n->"))
 
 #RECEBE LINKS DO YOUTUBE E EXIBIR INFORMAÇÕES
 
@@ -74,8 +78,6 @@ if(opc==2):
 		print(yt[i].streams.filter(mime_type="video/mp4"))
 		print("")
 		print("  Titulo: %s"%(yt[i].title))
-
-		res[i] = int(input("  \t 1 = Retorno em 720p\n2 = Retorno em 360p\n"))
 
 		temp = yt[i].length
 		h = temp/3600
@@ -95,34 +97,35 @@ if(opc==2):
 		i+=1
 
 #DOWNLOAD EM VIDEO
+	res = int(input("  \t 1 = Retorno em 720p\n2 = Retorno em 360p\n"))
 
 	i=1
 	while i<rep:
 		if(format==1):
 			print("")
-			if(res[i]==1):
+			if(res==1):
 				movie = yt[i].streams\
 				.filter(mime_type="video/mp4")\
 				.filter(res="720p")\
 				.first()\
 				.download(vid)
 
-			if(res[i]==2):
+			if(res==2):
 				movie = yt[i].streams\
 				.filter(mime_type="video/mp4")\
 				.filter(res="360p")\
 				.first()\
 				.download(vid)
-
-			print("Movie %d Salve" %(i))
+				
+			print('\n\tConcluido -> {} -> {} \n'.format(movie.title, movie.watch_url))
 #DOWNLOAD EM AUDIO
 
 		if(format==2):
 			print("")
 			audio = yt[i].streams.get_audio_only()
 			audio.download(vid)
-	
-			print("Audio %d Salve" %(i))
+			
+			print('\n\tConcluido -> {} -> {} \n'.format(audio.title, audio.watch_url))
 		i+=1
 	
 	print("\t\t%d DOWNLOADS CONCLUIDOS" %(rep-1))
